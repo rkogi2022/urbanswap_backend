fb3ebab717ea0fabeeee97930432ba3789c1fd39
Andrew Owino <andystudy4@gmail.com>
2024-05-16 20:08:58 +0300

    Remove unused imports

--- src/main/java/com/gloibgroup/urbanswap/controllers/CustomersController.java
+++ src/main/java/com/gloibgroup/urbanswap/controllers/CustomersController.java
@@ -4,21 +4,15 @@ import com.gloibgroup.urbanswap.dtos.payloads.ApiResponse;
 import com.gloibgroup.urbanswap.dtos.requests.CustomerSignupDTO;
 import com.gloibgroup.urbanswap.models.Customer;
 import com.gloibgroup.urbanswap.services.CustomerService;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.util.StringUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/customers")
