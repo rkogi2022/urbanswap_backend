ea0fb4cc548b78ec761a361d9b14068486c5246b
rkogi2022 <rhyttahkogi@gmail.com>
2024-05-21 13:10:10 +0300

    modified the Wallet Model

--- src/main/java/com/gloibgroup/urbanswap/models/Wallet.java
+++ src/main/java/com/gloibgroup/urbanswap/models/Wallet.java
@@ -1,7 +1,8 @@
 package com.gloibgroup.urbanswap.models;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.Column;
+import jakarta.persistence.Table;
+import lombok.*;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
@@ -9,12 +10,15 @@ import java.math.BigDecimal;
 import java.util.UUID;
 
 @Entity
-@Getter
-@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "wallets")
 public class Wallet {
 
     @Id
     private String id = UUID.randomUUID().toString();
+    @Column(name = "user_id")
     private String userId;
     private BigDecimal balance;
 
